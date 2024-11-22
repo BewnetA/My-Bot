d7d7a2f922c0601cf6bad52d0a8dae51a84cb50a
Bewnet <149056348+BewnetA@users.noreply.github.com>
2024-11-22 13:25:39 +0300

    Update bot.py
    threading added to await the process

--- bot.py
+++ bot.py
@@ -156,7 +156,7 @@ def choice_handler(call):
 
     else:
         temp_text = bot.send_message(chat_id, f"Downloading...")
-        download_from_youtube(call.data, call.message)
+        threading.Timer(5.0, lambda: download_from_youtube(call.data, call.message)).start()
         bot.delete_message(chat_id, temp_text.id)
 
 
